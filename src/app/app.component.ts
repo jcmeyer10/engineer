da95966bdda6183d81d2cc7034f10537a22310ea
Jeff Meyer <jcmeyer10@gmail.com>
2016-11-15 20:01:24 -0500

    ""

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,28 +15,20 @@ export class AppComponent {
     new Engineer("Bob", 0)
   ];
   myEng = this.engineers[0];
-
+  pickedEngineer = '';
 
   getEng(){
-    var target = event.srcElement || event.currentTarget;
-    console.log(target);
-    var idAttr = target.attributes;
-    console.log(idAttr);
 
     var number = Math.random() * 100;
 
     if (number > 0 &&  number <= 34) {
       var pickedEngineer = "Jim";
-      console.log(pickedEngineer);
-      return pickedEngineer;
     } else if (number > 34 && number <= 66 ){
       var pickedEngineer = "John";
-      console.log(pickedEngineer);
-      return pickedEngineer;
     } else {
       var pickedEngineer = "Bob";
-      console.log(pickedEngineer);
-      return pickedEngineer;
     }
+      this.pickedEngineer = pickedEngineer;
+      console.log(pickedEngineer);
   }
   }
